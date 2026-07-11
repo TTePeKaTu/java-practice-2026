cc5978995018e7222e0c9c4d42205a2522cb7b0e
TTePeKaTu <ebabushkin2020@gmail.com>
2026-07-11 06:05:59 +0300

    Task_3

--- Task 3/Schema.sql
+++ Task 3/Schema.sql
@@ -3,3 +3,6 @@ create table product (
     name char(50) not null,
     cost integer check(cost > -1)
 );
+insert into product (name, cost) values ('milk', 129);
+insert into product (name, cost) values ('dont milk', 921);
+insert into product (name, cost) values ('chocolate', 19);
